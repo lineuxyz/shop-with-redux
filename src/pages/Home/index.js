c3097fadc7f06f2375bb0180ee9283eb1fbabc66
Lineu Pastorelli <lineualberto@gmail.com>
2019-09-13 15:29:43 -0300

    created cart and home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,110 @@
 import React from 'react';
+import {MdShoppingCart} from 'react-icons/md';
 
-// import { Container } from './styles';
+import {ProductList} from './styles';
 
 export default function Home() {
-  return <div />;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>
+          Tênis muito legal com titulo muito grande que quebra varias linhas e
+          me da trabalho pra estilizar
+        </strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+          alt="Tênis"
+        />
+        <strong>Tênis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdShoppingCart size={16} color="#fff" /> 3
+          </div>
+
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
