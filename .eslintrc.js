cbb0569d07ea3127b55eecaeef4ff3101a13ed0f
Lineu Pastorelli <lineualberto@gmail.com>
2019-09-14 16:47:37 -0300

    added features to home and cart

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,11 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'airbnb',
-    'prettier',
-    'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
-    DEV: 'readonly'
+    DEV: 'readonly',
   },
   parser: 'babel-eslint',
   parserOptions: {
@@ -21,22 +17,16 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier'
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
     'react/static-property-placement': 'off',
     'react/jsx-props-no-spreading': 'off',
 
-    "prettier/prettier": "error",
+    'prettier/prettier': 'error',
     'react/state-in-constructor': 'off',
-    'react/jsx-filename-extension': [
-      'warn',
-      { extensions: ['.jsx', '.js'] }
-    ],
+    'react/jsx-filename-extension': ['warn', {extensions: ['.jsx', '.js']}],
     'import/prefer-default-export': 'off',
-    'no-param-reassign':'off',
-    'no-console':["error", {allow:["tron"]}]
+    'no-param-reassign': 'off',
+    'no-console': ['error', {allow: ['tron']}],
   },
-}
+};
